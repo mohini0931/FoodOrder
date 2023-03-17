db69b19df9a1435b7bb6351d64301effda33cfdc
Mohini7816 <mnarwade0931@gmail.com>
2023-03-17 17:40:22 +0400

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  foods: string[] = [];
+
+  constructor(private foodService: FoodService) { }
+
+  ngOnInit(): void {
+      this.foods = this.foodService.getAll();
+  }
+
+}
